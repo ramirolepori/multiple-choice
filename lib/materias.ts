f75189e8b4fdbf22676c53832a06974fad62fba3
ramirolepori <leporiramiro@gmail.com>
2026-06-03 00:48:08 -0300

    Fix materias import for new question bank

--- lib/materias.ts
+++ lib/materias.ts
@@ -1,5 +1,4 @@
-import matematicas from '../data/matematicas.json';
-import historia from '../data/historia.json';
+import redesDeDatos from '../data/Redes_de_Datos.json';
 import type { Materia } from '../types';
 
-export const materias: Materia[] = [matematicas, historia];
+export const materias: Materia[] = [redesDeDatos];
